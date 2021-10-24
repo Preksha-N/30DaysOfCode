812d3ecb85aceb4ad840df06ac0951fe2a268ffa
Preksha-N <prekshashetti517@gmail.com>
2021-10-24 18:59:28 +0530

    Add 6th day

---
+++ day6.java
@@ -0,0 +1,64 @@
+/**************************************************
+Problem statement
+ You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+ Example:candles=[4,3,4,1]
+
+ The maximum height candles are 4 units high. There are 2 of them, so return 2.
+   **************************************************/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        // Write your code here
+        int maxHeight=Collections.max(candles);
+        int numberOfTallestCandles = 0;
+
+        for(int i=0;i<candles.size();i++){
+            if(candles.get(i)==maxHeight){
+                numberOfTallestCandles++;
+            }
+        }
+        return numberOfTallestCandles;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
